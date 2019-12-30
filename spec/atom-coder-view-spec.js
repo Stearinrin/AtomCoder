e4e19c9408dbb49feb0301a4f62fdae641ea6e44
Stearin <jacker384sushilover@gmail.com>
2019-12-30 22:00:43 +0900

    First commit

---
+++ spec/atom-coder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCoderView from '../lib/atom-coder-view';
+
+describe('AtomCoderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
